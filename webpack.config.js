3222abecbc88379e13ab12982ae406b403000f0d
Mofles11 <luisangelramosmejia11@gmail.com>
2022-05-31 20:54:20 -0500

    [add] index.js and configure react app

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,46 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+
 module.exports = {
-  mode: "development",
-  output: {
-    filename: "app.bundle.js",
-    publicPath: "/",
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "src/index.html",
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env", "@babel/preset-react"],
-          },
-        },
-      },
-    ],
-  },
-};
+	mode: 'development',
+	output: {
+		filename: 'app.bundle.js',
+		publicPath: '/',
+	},
+	resolve: {
+		extensions: ['.js', '.jsx'],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: 'public/index.html',
+		}),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env', '@babel/preset-react'],
+					},
+				},
+			},
+			{
+				test: /\.html$/,
+				use: [
+					{
+						loader: 'html-loader',
+					},
+				],
+			},
+		],
+	},
+	devServer: {
+		static: path.join(__dirname, 'dist'),
+		compress: true,
+		historyApiFallback: true,
+		port: 3005,
+	},
+}
